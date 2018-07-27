fdad917b09de060d0e2f5d1a27a3071f49228272
Will Skora <skorasaurus@gmail.com>
2018-07-27 12:21:05 -0400

    add missing event parameter for calculator

--- static/calculator.js
+++ static/calculator.js
@@ -87,7 +87,7 @@ function rerender() {
 }
 
 function init() {
-  document.querySelector(".calc-buttons").addEventListener("click", function() {
+  document.querySelector(".calc-buttons").addEventListener("click", function(event) {
     buttonClick(event.target.innerText);
   });
 }
